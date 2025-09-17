8a7592126915c4ca6a9f39805b82de818b9cd191
davasetiawan <davasetiawan893@gmail.com>
2025-09-17 08:49:51 +0700

    Add files via upload

---
+++ praktikum.py
@@ -0,0 +1,44 @@
+a = 4
+b = a + 5
+print (b)
+#string
+contoh_teks = "halo halo" 
+print(contoh_teks)
+
+
+#list
+list = [1, 2, 3, 4, 5]
+print(list)
+
+
+#list kedua 
+list_kedua = ["satu", "dua", "tiga"]
+contohkedua = list_kedua[1]
+print(contohkedua)
+
+
+#list ketiga
+list_ketiga = [[1,2,3],"satu","dua","tiga"]
+outputketiga = list_ketiga[0][2]
+print(outputketiga)
+
+
+#set
+set = {1, 2, 3, 4, 5,5,5,5}
+print(set)
+
+
+#dictionary
+harga = {'apel': 5000, 'jeruk': 7000, 'mangga': 20000}
+print(harga['jeruk'])
+list_buah = list(harga.keys())
+print(list_buah)
+list_harga = list(harga.values())
+print(list_harga)
+
+
+daftar_harga = {
+    'apel': 5000,
+    'jeruk': 7000,
+    'mangga': 20000
+}
